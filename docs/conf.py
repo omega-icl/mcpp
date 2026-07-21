1e8934988324a7f5d3eef237fe242b64eddc1db1
Kai Ruth <kr326ubuntu@ic-czc0278sym.dept.ic.ac.uk>
2026-07-21 18:10:46 +0100

    add copy button for code cells in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "myst_nb",
+    "sphinx_copybutton",
 ]
 
 # The tutorial notebooks live in notebook/ at the repository root; they are
